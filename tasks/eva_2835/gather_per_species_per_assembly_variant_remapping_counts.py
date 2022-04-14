0c7fff9d7b9b77c27ff146aaa3f151323131319b
Avirup Guha Neogi <s0avguha@uni-bonn.de>
2022-04-14 13:23:45 +0100

    Adding taxid, assemble function

--- tasks/eva_2835/gather_per_species_per_assembly_variant_remapping_counts.py
+++ tasks/eva_2835/gather_per_species_per_assembly_variant_remapping_counts.py
@@ -1,4 +1,4 @@
-#Copyright 2022 EMBL - European Bioinformatics Institute
+# Copyright 2022 EMBL - European Bioinformatics Institute
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,19 +13,49 @@
 # limitations under the License.
 
 # Importing relevant packages
+import os
 import yaml
 import argparse
 
-# Defining the function to store the counts of the remapped variants along wih the reason for failure
-# in different rounds
+
+def collect_taxid_assembly(remapping_root_path, output_path):
+    """
+    This function is used to store the ful list of taxonomy ids and assembly accessions
+    Input: It accepts the remapping full path from the user along with the output path
+           for storing the results for subsequent analysis
+    Output: It calls gather_counts_per_tax_per_assembly() function
+           for a particular taxonomy id and a particular assembly of that taxonomy
+    """
+
+    # Collecting the tax_ids from the input path
+    taxid = [name for name in os.listdir(remapping_root_path) if os.path.isdir(os.path.join(remapping_root_path, name))]
+
+    # Defining a dictionary to store the taxonomy and its corresponding assemblies
+    tax_assembly = {}
+
+    for i in taxid:
+        assembly_accession = [name for name in os.listdir(os.path.join(remapping_root_path, i)) if
+                              os.path.isdir(os.path.join(os.path.join(remapping_root_path, i), name))]
+        tax_assembly[i] = assembly_accession
+
+    # Generating statistics for each taxonomy and each assembly
+    for k, v in tax_assembly.items():
+        for i in range(len(v)):
+            gather_counts_per_tax_per_assembly(remapping_root_path, k, i)
 
 # In Progress
 
 
-def gather_counts_per_tax_per_assembly(remapping_root_path, output_file):
+def gather_counts_per_tax_per_assembly(path, taxid, assembly_accession):
+    """
+    This function is used to store the counts of the remapped variants along wih the reason for failures
+    at different rounds (i.e. for different lengths of the flanking region)
+    Input: The taxonomy id and the assembly accession from collect_taxid_assembly()
+    Output: A spreadsheet outlining all the relevant counts for each taxonomy and each assembly
+            per taxonomy
+    """
 
-
-    filename = assembly_accession + "_eva_remapped_counts.yml"
+    filename = os.path.join(path, taxid) + "_eva_remapped_counts.yml"
 
     with open(filename, 'r') as file:
 
@@ -67,7 +97,6 @@ def gather_counts_per_tax_per_assembly(remapping_root_path, output_file):
 # Defining the main function
 
 def main():
-
     parser = argparse.ArgumentParser(
         description='Collecting statistics per taxonomy per assembly for variant remapping')
     parser.add_argument("--remapping_root_path", type=str,
@@ -76,7 +105,7 @@ def main():
                         help="Path to the output .", required=True)
 
     args = parser.parse_args()
-    gather_counts_per_tax_per_assembly(args.remapping_root_path, args.output_file)
+    collect_taxid_assembly(args.remapping_root_path, args.output_file)
 
 
 if __name__ == "__main__":
