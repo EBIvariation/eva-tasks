554655c72e85bfb7e32c9ff86dcf969b13d5a394
Avirup Guha Neogi <s0avguha@uni-bonn.de>
2022-04-22 06:51:29 +0100

    Implementing gathering counts using the dicitonary approach

--- tasks/eva_2835/gather_per_species_per_assembly_variant_remapping_counts.py
+++ tasks/eva_2835/gather_per_species_per_assembly_variant_remapping_counts.py
@@ -16,6 +16,18 @@
 import os
 import yaml
 import argparse
+import csv
+
+# Initializing the final output column
+output_columns = []
+
+# Initializing the list to store all the keys and values for all the files
+all_keys = []
+all_values = []
+
+# Initializing the list to store the values of all taxonomy ids and assembly accessions
+all_tax_id =[]
+all_assembly = []
 
 
 def collect_taxid_assembly(remapping_root_path, output_path):
@@ -28,15 +40,15 @@ def collect_taxid_assembly(remapping_root_path, output_path):
     """
 
     # Collecting the tax_ids from the input path
-    taxid = [name for name in os.listdir(remapping_root_path) if os.path.isdir(os.path.join(remapping_root_path, name))]
+    taxids = [name for name in os.listdir(remapping_root_path) if os.path.isdir(os.path.join(remapping_root_path, name))]
 
     # Defining a dictionary to store the taxonomy and its corresponding assemblies
     tax_assembly = {}
 
-    for i in taxid:
-        assembly_accession = [name for name in os.listdir(os.path.join(remapping_root_path, i)) if
-                              os.path.isdir(os.path.join(os.path.join(remapping_root_path, i), name))]
-        tax_assembly[i] = assembly_accession
+    for tax_id in taxids:
+        assembly_accession = [name for name in os.listdir(os.path.join(remapping_root_path, tax_id)) if
+                              os.path.isdir(os.path.join(os.path.join(remapping_root_path, tax_id), name))]
+        tax_assembly[tax_id] = assembly_accession
 
     # Generating statistics for each taxonomy and each assembly
     for k, v in tax_assembly.items():
@@ -52,67 +64,115 @@ def gather_counts_per_tax_per_assembly(path, taxid, assembly_accession):
     at different rounds (i.e. for different lengths of the flanking region)
     Input: The taxonomy id and the assembly accession from collect_taxid_assembly()
     Output: A spreadsheet outlining all the relevant counts for each taxonomy and each assembly
-            per taxonomy
+            per taxonomy for both EVA and dbSNP data
     """
 
-    # Setting the filename
-    filename = assembly_accession + "_eva_remapped_counts.yml"
-    filename = os.path.join(path, str(taxid), assembly_accession, "eva", filename)
+    # Setting the filename for the eva counts
+    filename_eva = assembly_accession + "_eva_remapped_counts.yml"
+    filename_eva = os.path.join(path, str(taxid), assembly_accession, "eva", filename_eva)
 
-    with open(filename, 'r') as file:
+    # Setting the filename for the dbsnp counts
+    filename_dbsnp = assembly_accession + "_dbsnp_remapped_counts.yml"
+    filename_dbsnp = os.path.join(path, str(taxid), assembly_accession, "dbsnp", filename_dbsnp)
+
+    with open(filename_eva, 'r') as file:
 
         # Loading the data from the yaml file
         data = yaml.safe_load(file)
 
-        # Recording the total number of variants
-        temp = "all_" + str(taxid) + "_" + assembly_accession
-        locals[temp] = data['all']
+        # Storing the contents for a particular yaml file in a linear dictionary format for eva
+        keys = []
+        values = []
+        keys_values = {}
 
-        temp = "filtered_" + str(taxid) + "_" + assembly_accession
-        locals[temp] = data['filtered']
+        for key, value in data.items():
 
-        # Recording the remapping statistics for flanking region of length 50
-        temp_50 = "flank_50" + str(taxid) + "_" + assembly_accession
-        locals[temp] = data['Flank_50']
+            # Checking if the value is dictionary
+            if isinstance(value, dict):
+                for key2, value2 in value.items():
+                    keys.append(key + "_" + key2)
+                    values.append(value2)
+            else:
+                keys.append(key.capitalize())
+                values.append(value)
 
-        # Recording the remapping statistics for flanking region of length 2000
-        temp_2000 = "flank_2000" + str(taxid) + "_" + assembly_accession
-        locals[temp] = data['Flank_2000']
+        # Creating dictionary from two lists
+        zip_iterator = zip(keys, values)
+        keys_values = dict(zip_iterator)
 
-        # Recording the remapping statistics for flanking region of length 50000
-        temp_50000 = "flank_50000" + str(taxid) + "_" + assembly_accession
-        locals[temp] = data['Flank_50000']
+    with open(filename_dbsnp, 'r') as file:
 
-        # Collecting failure statistics for flanking region of length 50
-        for k, v in temp_50.items():
-            k = k.replace(" ", "")
-            temp = "flank_50_" + k + "_taxid_assembly_accession"
-            locals()[temp] = v
+        # Loading the data from the yaml file
+        data = yaml.safe_load(file)
 
-        # Collecting failure statistics for flanking region of length 2000
-        for k, v in temp_2000.items():
-            k = k.replace(" ", "")
-            temp = "flank_2000_" + k + "_taxid_assembly_accession"
-            locals()[temp] = v
+        # Storing the contents for a particular yaml file in a linear dictionary format for dbsnp
+        keys = []
+        values = []
+        keys_values_dbsnp = {}
 
-        # Collecting failure statistics for flanking region of length 50000
-        for k, v in temp_50000.items():
-            k = k.replace(" ", "")
-            temp = "flank_50_" + k + "_taxid_assembly_accession"
-            locals()[temp] = v
+        for key, value in data.items():
 
+            # Checking if the value is dictionary
+            if isinstance(value, dict):
+                for key2, value2 in value.items():
+                    keys.append(key + "_" + key2)
+                    values.append(value2)
+            else:
+                keys.append(key.capitalize())
+                values.append(value)
 
-# Defining the main function
+        # Creating dictionary from two lists
+        zip_iterator = zip(keys, values)
+        keys_values_dbsnp = dict(zip_iterator)
+
+    # Adding the values of bdsnp and eva with the common keys
+    for key in keys_values_dbsnp:
+        if key in keys_values:
+            keys_values[key] = keys_values[key] + keys_values_dbsnp[key]
+        else:
+            pass
+
+    # Sorting the dictionary using the keys
+    keys_values = {key: value for key, value in sorted(keys_values.items())}
+
+    # Splitting the dictionary into two lists - one for keys and one for values
+    keys, values = zip(*keys_values.items())
+    keys = list(keys)
+    values = list(values)
+
+    # Adding the keys and values list to the master list for each taxononmy and assembly
+    all_keys.append(keys)
+    all_values.append(values)
+
+    # Adding the taxonomy and assembly accession for a particular file
+    all_tax_id.append(taxid)
+    all_assembly.append(assembly_accession)
+
+    # Updating the final output columns
+    output_columns.extend(keys)
+    output_columns = list(dict.fromkeys(output_columns))
+
+# Defining the main function to accept the inputs from the user
+
 
 def main():
     parser = argparse.ArgumentParser(
         description='Collecting statistics per taxonomy per assembly for variant remapping')
+
+    # Allowing the user to enter the full path for the taxonomy folders which contain
+    # the corresponding old assemblies which in turn contain the yaml files containing
+    # the remapped statistics
     parser.add_argument("--remapping_root_path", type=str,
                         help="Path where the remapping directories are present", required=True)
+
+    # Taking the output path from the user where the visualization files and output csv
+    # files are present
     parser.add_argument("--output_file", type=str,
                         help="Path to the output .", required=True)
 
     args = parser.parse_args()
+
+    # Calling the primary function responsible for generating the statistics
     collect_taxid_assembly(args.remapping_root_path, args.output_file)
 
 
