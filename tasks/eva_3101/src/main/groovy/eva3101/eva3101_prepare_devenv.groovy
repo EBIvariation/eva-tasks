115cee4b5be720ac9cdd476bb33e819760508cdd
sundarvenkata-ebi <sundarvenkata@ebi.ac.uk>
2023-01-04 01:29:39 +0000

    Remove unneeded imports and update script comment

--- tasks/eva_3101/src/main/groovy/eva3101/eva3101_prepare_devenv.groovy
+++ tasks/eva_3101/src/main/groovy/eva3101/eva3101_prepare_devenv.groovy
@@ -1,35 +1,14 @@
 package eva3101
 
 import groovy.cli.picocli.CliBuilder
-import org.springframework.batch.core.JobParameter
-import org.springframework.batch.core.JobParameters
-import org.springframework.batch.core.StepExecutionListener
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
-import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.batch.core.launch.support.RunIdIncrementer
-import org.springframework.batch.core.repository.JobRepository
-import org.springframework.batch.item.ExecutionContext
-import org.springframework.batch.item.ItemStreamException
-import org.springframework.batch.item.ItemStreamReader
-import org.springframework.batch.item.NonTransientResourceException
-import org.springframework.batch.item.ParseException
-import org.springframework.batch.item.UnexpectedInputException
-import org.springframework.batch.repeat.policy.SimpleCompletionPolicy
-import org.springframework.transaction.PlatformTransactionManager
-import uk.ac.ebi.eva.accession.core.batch.io.SubmittedVariantDeprecationWriter
-import uk.ac.ebi.eva.accession.core.model.eva.SubmittedVariantEntity
 import uk.ac.ebi.eva.accession.deprecate.Application
-import uk.ac.ebi.eva.accession.deprecate.batch.listeners.DeprecationStepProgressListener
 import uk.ac.ebi.eva.groovy.commons.RetryableBatchingCursor
-import uk.ac.ebi.eva.metrics.metric.MetricCompute
-
-import java.time.LocalDateTime
 
 import static uk.ac.ebi.eva.groovy.commons.EVADatabaseEnvironment.*
 import static org.springframework.data.mongodb.core.query.Criteria.where
 
-// This script deprecates variants with allelesMatch attribute in dbsnpSVE collection and also the corresponding RS (if any)
+// This script populates the development environment test database with
+// variants having allelesMatch attribute false in dbsnpSVE collection
 def cli = new CliBuilder()
 cli.prodPropertiesFile(args: 1, "Production properties file for accessioning", required: true)
 cli.devPropertiesFile(args: 1, "Development properties file for accessioning", required: true)
